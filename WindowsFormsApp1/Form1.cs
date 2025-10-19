f345d66582b2f4c06297a7736e3502c931cfc1c5
Metehan Cetiner <eametehan10@gmail.com>
2025-10-19 16:19:09 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        SqlConnection connection = new SqlConnection(@"Server=[YOUR-SERVER-NAME]\SQLEXPRESS;Database = [YOUR-DB]; Integrated Security=True");
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string strpassword = richTextBox2.Text;
+                PasswordHasher hasher = new PasswordHasher();
+                string hashedpassword = hasher.HashPassword(strpassword);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO [YOUR-TABLE-NAME] (Username,Password) VALUES (@username,@password)", connection);
+                cmd.Parameters.AddWithValue("@username", richTextBox1.Text);
+                cmd.Parameters.AddWithValue("@password", hashedpassword);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Your informations successfully saved!","Information", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                Form2 loginForm = new Form2();
+                loginForm.Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured!", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+            
+            
+            
+            private void button2_Click(object sender, EventArgs e)
+            {
+                
+
+            }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/PasswordHasher.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp1 
+{
+    public class PasswordHasher
+    {
+        private const int Iterations = 100000;
+        private const int SaltSize = 16;
+        private const int HashSize = 32;
+
+        public string HashPassword(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations);
+            byte[] hash = pbkdf2.GetBytes(HashSize);
+
+            byte[] hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public bool VerifyPassword(string password, string hashedPasswordFromDb)
+        {
+            
+            byte[] hashBytes = Convert.FromBase64String(hashedPasswordFromDb);
+            byte[] salt = new byte[SaltSize];
+            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+            byte[] storedHash = new byte[HashSize];
+            Array.Copy(hashBytes, SaltSize, storedHash, 0, HashSize);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations);
+            byte[] newHash = pbkdf2.GetBytes(HashSize);
+
+            bool success = true;
+            for (int i = 0; i < HashSize; i++)
+            {
+                if (storedHash[i] != newHash[i])
+                {
+                    success = false;
+                }
+            }
+            return success;
+        }
+    }
+}
